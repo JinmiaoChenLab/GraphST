d75e390bafb95e81681887ba096c9b31c6714db7
longyahui <60100581+longyahui@users.noreply.github.com>
2022-07-25 16:18:35 +0800

    Update __init__.py

--- DeepST/__init__.py
+++ DeepST/__init__.py
@@ -8,7 +8,5 @@
 __author__ = "Yahui Long"
 __email__ = "long_yahui@immunol.a-star.edu.sg"
 
-from .Train_STAGATE import train_STAGATE
-from .utils import Cal_Spatial_Net, Stats_Spatial_Net, mclust_R, Cal_Spatial_Net_3D
+from .utils import clustering
 from .preprocess import preprocess_adj, preprocess, construct_interaction, add_contrastive_label, get_feature, permutation, fix_seed
-from .
