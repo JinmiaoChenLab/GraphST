573a36515e36b1f81e3430a6ba7cf012137afb95
longyahui <60100581+longyahui@users.noreply.github.com>
2022-07-26 16:47:16 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,4 +9,4 @@ __author__ = "Yahui Long"
 __email__ = "long_yahui@immunol.a-star.edu.sg"
 
 from .utils import clustering
-#from .preprocess import preprocess_adj, preprocess, construct_interaction, add_contrastive_label, get_feature, permutation, fix_seed
+from .preprocess import preprocess_adj, preprocess, construct_interaction, add_contrastive_label, get_feature, permutation, fix_seed, filter_with_overlap_gene
