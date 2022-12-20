ed1ab43496b64bcedf7bea002fb41920da9ec85d
longyahui <60100581+longyahui@users.noreply.github.com>
2022-12-20 16:57:30 +0800

    Update __init__.py

--- DeepST/__init__.py
+++ DeepST/__init__.py
@@ -9,4 +9,4 @@ __author__ = "Yahui Long"
 __email__ = "long_yahui@immunol.a-star.edu.sg"
 
 from .utils import clustering, project_cell_to_spot
-from .preprocess import preprocess_adj, preprocess, construct_interaction, add_contrastive_label, get_feature, permutation, fix_seed
+from .preprocess import preprocess_adj, preprocess, construct_interaction, construct_interaction_KNN, add_contrastive_label, get_feature, permutation, fix_seed
