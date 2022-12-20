00c70b9eb909719ebec19dc1e2f489a21b3f688b
longyahui <60100581+longyahui@users.noreply.github.com>
2022-12-20 17:53:50 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import Command, find_packages, setup
 
-__lib_name__ = "DeepST"
+__lib_name__ = "GraphST"
 __lib_version__ = "1.0.0"
-__description__ = "DeepST: A versatile graph contrastive learning framework for spatially informed clustering, integration and transfer of spatial transcriptomics"
-__url__ = "https://github.com/longyahui/DeepST"
+__description__ = "Spatially informed clustering, integration, and deconvolution of spatial transcriptomics with GraphST"
+__url__ = "https://github.com/longyahui/GraphST"
 __author__ = "Yahui Long"
 __author_email__ = "longyh@immunol.a-star.edu.sg"
 __license__ = "MIT"
-__keywords__ = ["spatial transcriptomics", "Deep learning", "Graph neural networks", "Contrastive learning", "Self-supervised learning"]
+__keywords__ = ["Spatial transcriptomics", "Graph self-supervised contrastive learning", "Spatial clustering", "Cell type deconvolution", "Batch correction", "Single-cell RNA-seq"]
 __requires__ = ["requests",]
 
 with open("README.rst", "r", encoding="utf-8") as f:
@@ -21,7 +21,7 @@ setup(
     author = __author__,
     author_email = __author_email__,
     license = __license__,
-    packages = ['DeepST'],
+    packages = ['GraphST'],
     install_requires = __requires__,
     zip_safe = False,
     include_package_data = True,
