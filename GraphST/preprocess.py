d40b0d4c3d2ad15d1fd948aae1ddc73feeb2a917
longyahui <60100581+longyahui@users.noreply.github.com>
2022-12-20 17:32:36 +0800

    Add files via upload

---
+++ GraphST/preprocess.py
@@ -0,0 +1,193 @@
+import os
+import torch
+import random
+import numpy as np
+import scanpy as sc
+import scipy.sparse as sp
+from torch.backends import cudnn
+#from scipy.sparse import issparse
+from scipy.sparse.csc import csc_matrix
+from scipy.sparse.csr import csr_matrix
+from sklearn.neighbors import NearestNeighbors 
+
+def filter_with_overlap_gene(adata, adata_sc):
+    # remove all-zero-valued genes
+    #sc.pp.filter_genes(adata, min_cells=1)
+    #sc.pp.filter_genes(adata_sc, min_cells=1)
+    
+    if 'highly_variable' not in adata.var.keys():
+       raise ValueError("'highly_variable' are not existed in adata!")
+    else:    
+       adata = adata[:, adata.var['highly_variable']]
+       
+    if 'highly_variable' not in adata_sc.var.keys():
+       raise ValueError("'highly_variable' are not existed in adata_sc!")
+    else:    
+       adata_sc = adata_sc[:, adata_sc.var['highly_variable']]   
+
+    # Refine `marker_genes` so that they are shared by both adatas
+    genes = list(set(adata.var.index) & set(adata_sc.var.index))
+    genes.sort()
+    print('Number of overlap genes:', len(genes))
+
+    adata.uns["overlap_genes"] = genes
+    adata_sc.uns["overlap_genes"] = genes
+    
+    adata = adata[:, genes]
+    adata_sc = adata_sc[:, genes]
+    
+    return adata, adata_sc
+
+def permutation(feature):
+    # fix_seed(FLAGS.random_seed) 
+    ids = np.arange(feature.shape[0])
+    ids = np.random.permutation(ids)
+    feature_permutated = feature[ids]
+    
+    return feature_permutated
+ 
+
+def calculate_distance(x):
+    """Compute pairwise Euclidean distances.
+    """
+    assert isinstance(x, np.ndarray) and x.ndim == 2
+
+    x_square = np.expand_dims(np.einsum('ij,ij->i', x, x), axis=1)
+    y_square = x_square.T
+    
+    distances = np.dot(x, x.T)
+    distances *= -2
+    distances += x_square
+    distances += y_square
+    
+    # Ensure all values are larger than 0
+    np.maximum(distances, 0, distances)
+    
+    # Ensure that self-distance is set to 0.0 
+    distances.flat[::distances.shape[0] + 1] = 0.0
+    
+    np.sqrt(distances, distances)
+    
+    return distances 
+
+def construct_interaction(adata, n_neighbors=3):
+    """Constructing spot-to-spot interactive graph"""
+    position = adata.obsm['spatial']
+    # calculate distance matrix
+    distance_matrix = calculate_distance(position.astype(np.float64))
+    n_spot = distance_matrix.shape[0]
+    
+    adata.obsm['distance_matrix'] = distance_matrix
+    
+    # find k-nearest neighbors
+    interaction = np.zeros([n_spot, n_spot])  
+    for i in range(n_spot):
+        vec = distance_matrix[i, :]
+        distance = vec.argsort()
+        for t in range(1, n_neighbors + 1):
+            y = distance[t]
+            interaction[i, y] = 1
+         
+    adata.obsm['graph_neigh'] = interaction
+    
+    #transform adj to symmetrical adj
+    adj = interaction
+    adj = adj + adj.T
+    adj = np.where(adj>1, 1, adj)
+    
+    adata.obsm['adj'] = adj
+    
+def construct_interaction_KNN(adata, n_neighbors=3):
+    position = adata.obsm['spatial']
+    n_spot = position.shape[0]
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors+1).fit(position)  
+    _ , indices = nbrs.kneighbors(position)
+    x = indices[:, 0].repeat(n_neighbors)
+    y = indices[:, 1:].flatten()
+    interaction = np.zeros([n_spot, n_spot])
+    interaction[x, y] = 1
+    
+    adata.obsm['graph_neigh'] = interaction
+    
+    #transform adj to symmetrical adj
+    adj = interaction
+    adj = adj + adj.T
+    adj = np.where(adj>1, 1, adj)
+    
+    adata.obsm['adj'] = adj
+    print('Graph constructed!')   
+
+def preprocess(adata):
+    sc.pp.highly_variable_genes(adata, flavor="seurat_v3", n_top_genes=3000)
+    sc.pp.normalize_total(adata, target_sum=1e4)
+    sc.pp.log1p(adata)
+    sc.pp.scale(adata, zero_center=False, max_value=10)
+    
+def get_feature(adata, deconvolution=False):
+    if deconvolution:
+       adata_Vars = adata
+    else:   
+       adata_Vars =  adata[:, adata.var['highly_variable']]
+       
+    if isinstance(adata_Vars.X, csc_matrix) or isinstance(adata_Vars.X, csr_matrix):
+       feat = adata_Vars.X.toarray()[:, ]
+    else:
+       feat = adata_Vars.X[:, ] 
+    
+    # data augmentation
+    feat_a = permutation(feat)
+    
+    adata.obsm['feat'] = feat
+    adata.obsm['feat_a'] = feat_a    
+    
+def add_contrastive_label(adata):
+    # contrastive label
+    n_spot = adata.n_obs
+    one_matrix = np.ones([n_spot, 1])
+    zero_matrix = np.zeros([n_spot, 1])
+    label_CSL = np.concatenate([one_matrix, zero_matrix], axis=1)
+    adata.obsm['label_CSL'] = label_CSL
+    
+def normalize_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    adj = adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt)
+    return adj.toarray()
+
+def preprocess_adj(adj):
+    """Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
+    adj_normalized = normalize_adj(adj)+np.eye(adj.shape[0])
+    return adj_normalized 
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+def preprocess_adj_sparse(adj):
+    adj = sp.coo_matrix(adj)
+    adj_ = adj + sp.eye(adj.shape[0])
+    rowsum = np.array(adj_.sum(1))
+    degree_mat_inv_sqrt = sp.diags(np.power(rowsum, -0.5).flatten())
+    adj_normalized = adj_.dot(degree_mat_inv_sqrt).transpose().dot(degree_mat_inv_sqrt).tocoo()
+    return sparse_mx_to_torch_sparse_tensor(adj_normalized)    
+
+def fix_seed(seed):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    cudnn.deterministic = True
+    cudnn.benchmark = False
+    
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8' 
---
+++ GraphST/utils.py
@@ -0,0 +1,210 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.decomposition import PCA
+
+
+def mclust_R(adata, num_cluster, modelNames='EEE', used_obsm='emb_pca', random_seed=2020):
+    """\
+    Clustering using the mclust algorithm.
+    The parameters are the same as those in the R package mclust.
+    """
+    
+    np.random.seed(random_seed)
+    import rpy2.robjects as robjects
+    robjects.r.library("mclust")
+
+    import rpy2.robjects.numpy2ri
+    rpy2.robjects.numpy2ri.activate()
+    r_random_seed = robjects.r['set.seed']
+    r_random_seed(random_seed)
+    rmclust = robjects.r['Mclust']
+    
+    res = rmclust(rpy2.robjects.numpy2ri.numpy2rpy(adata.obsm[used_obsm]), num_cluster, modelNames)
+    mclust_res = np.array(res[-2])
+
+    adata.obs['mclust'] = mclust_res
+    adata.obs['mclust'] = adata.obs['mclust'].astype('int')
+    adata.obs['mclust'] = adata.obs['mclust'].astype('category')
+    return adata
+
+def clustering(adata, n_clusters=7, radius=50, key='emb', threshold=0.06, datatype='10X', sample='single', refinement=False):
+    """\
+    Spatial clustering based the learned representation.
+
+    Parameters
+    ----------
+    adata : anndata
+        AnnData object of scanpy package.
+    n_clusters : int, optional
+        The number of clusters. The default is 7.
+    radius : int, optional
+        The number of neighbors considered during refinement. The default is 50.
+    key : string, optional
+        The key of the learned representation in adata.obsm. The default is 'emb'.
+    threshold : float, optional
+        Cutoff for selecting the final labels. For 10X Visium data, the model is trained twice,
+        i.e., with and without penalty terms. As a result,  two clustering label results corresponding to the
+        two-time training are generated. The final clustering label is determined by Silhouette score. 
+        The default is 0.06.
+    data_type : string, optional
+        Data type of input spatial data. The current version of DeepST can be applied to different ST data,
+        including 10X Visium ('10X'), Stereo-seq ('Stereo'), and Slide-seq/Slide-seqV2 ('Slide').
+    sample: string, optional
+        The number of input ST data. 'single' means single ST sample; 'multiple' means multiple ST samples (i.e., ST data integration task)      
+    refinement : bool, optional
+        Refine the predicted labels or not. The default is True.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    pca = PCA(n_components=20, random_state=42) 
+    
+    if datatype == '10X' and sample=='single':
+       # clustering 1
+       embedding = pca.fit_transform(adata.obsm['emb'].copy())
+       adata.obsm['emb_pca'] = embedding
+       adata = mclust_R(adata, used_obsm='emb_pca', num_cluster=n_clusters)
+       adata.obs['label'] = adata.obs['mclust']
+       new_type = refine_label(adata, radius, key='label')
+       adata.obs['label_refined'] = new_type
+    
+       # clustering 2
+       embedding = pca.fit_transform(adata.obsm['emb_reg'].copy())
+       adata.obsm['emb_reg_pca'] = embedding
+       adata = mclust_R(adata, used_obsm='emb_reg_pca', num_cluster=n_clusters)
+       adata.obs['label_reg'] = adata.obs['mclust']
+       new_type = refine_label(adata, radius, key='label_reg')
+       adata.obs['label_reg_refined'] = new_type
+    
+       # Silhouette
+       SIL = metrics.silhouette_score(adata.obsm['emb_pca'], adata.obs['label'], metric='euclidean')
+       SIL_reg = metrics.silhouette_score(adata.obsm['emb_reg_pca'], adata.obs['label_reg'], metric='euclidean')
+    
+       if abs(SIL-SIL_reg) > threshold and SIL_reg > SIL:
+          if refinement: 
+             adata.obs['domain'] = adata.obs['label_reg_refined']
+          else:   
+             adata.obs['domain'] = adata.obs['label_reg']
+       else:
+          if refinement: 
+             adata.obs['domain'] = adata.obs['label_refined']
+          else:
+             adata.obs['domain'] = adata.obs['label']
+             
+    elif datatype in ['Stereo', 'Slide'] or sample=='multiple':
+         embedding = pca.fit_transform(adata.obsm['emb'].copy())
+         adata.obsm['emb_pca'] = embedding
+         adata = mclust_R(adata, used_obsm='emb_pca', num_cluster=n_clusters)
+         adata.obs['label'] = adata.obs['mclust'] 
+       
+def refine_label(adata, radius=50, key='label'):
+    n_neigh = radius
+    new_type = []
+    old_type = adata.obs[key].values
+    
+    #read distance
+    if 'distance_matrix' not in adata.obsm.keys():
+        raise ValueError("Distance matrix is not existed!")
+    distance = adata.obsm['distance_matrix'].copy()
+           
+    n_cell = distance.shape[0]
+    
+    for i in range(n_cell):
+        vec  = distance[i, :]
+        index = vec.argsort()
+        neigh_type = []
+        for j in range(1, n_neigh+1):
+            neigh_type.append(old_type[index[j]])
+        max_type = max(neigh_type, key=neigh_type.count)
+        new_type.append(max_type)
+        
+    new_type = [str(i) for i in list(new_type)]    
+    #adata.obs['label_refined'] = np.array(new_type)
+    
+    return new_type
+
+def extract_top_value(map_matrix, retain_percent = 0.1): 
+    '''\
+    Filter out cells with low mapping probability
+
+    Parameters
+    ----------
+    map_matrix : array
+        Mapped matrix with m spots and n cells.
+    retain_percent : float, optional
+        The percentage of cells to retain. The default is 0.1.
+
+    Returns
+    -------
+    output : array
+        Filtered mapped matrix.
+
+    '''
+
+    #retain top 1% values for each spot
+    top_k  = retain_percent * map_matrix.shape[1]
+    output = map_matrix * (np.argsort(np.argsort(map_matrix)) >= map_matrix.shape[1] - top_k)
+    
+    return output 
+
+def construct_cell_type_matrix(adata_sc):
+    label = 'cell_type'
+    n_type = len(list(adata_sc.obs[label].unique()))
+    zeros = np.zeros([adata_sc.n_obs, n_type])
+    cell_type = list(adata_sc.obs[label].unique())
+    cell_type = [str(s) for s in cell_type]
+    cell_type.sort()
+    mat = pd.DataFrame(zeros, index=adata_sc.obs_names, columns=cell_type)
+    for cell in list(adata_sc.obs_names):
+        ctype = adata_sc.obs.loc[cell, label]
+        mat.loc[cell, str(ctype)] = 1
+    #res = mat.sum()
+    return mat
+
+def project_cell_to_spot(adata, adata_sc, retain_percent=0.1):
+    '''\
+    Project cell types onto ST data using mapped matrix in adata.obsm
+
+    Parameters
+    ----------
+    adata : anndata
+        AnnData object of spatial data.
+    adata_sc : anndata
+        AnnData object of scRNA-seq reference data.
+    retrain_percent: float    
+        The percentage of cells to retain. The default is 0.1.
+    Returns
+    -------
+    None.
+
+    '''
+    
+    # read map matrix 
+    map_matrix = adata.obsm['map_matrix']   # spot x cell
+   
+    # extract top-k values for each spot
+    map_matrix = extract_top_value(map_matrix) # filtering by spot
+    
+    # construct cell type matrix
+    matrix_cell_type = construct_cell_type_matrix(adata_sc)
+    matrix_cell_type = matrix_cell_type.values
+       
+    # projection by spot-level
+    matrix_projection = map_matrix.dot(matrix_cell_type)
+   
+    # rename cell types
+    cell_type = list(adata_sc.obs['cell_type'].unique())
+    cell_type = [str(s) for s in cell_type]
+    cell_type.sort()
+    #cell_type = [s.replace(' ', '_') for s in cell_type]
+    df_projection = pd.DataFrame(matrix_projection, index=adata.obs_names, columns=cell_type)  # spot x cell type
+    
+    #normalize by row (spot)
+    df_projection = df_projection.div(df_projection.sum(axis=1), axis=0).fillna(0)
+
+    #add projection results to adata
+    adata.obs[df_projection.columns] = df_projection
