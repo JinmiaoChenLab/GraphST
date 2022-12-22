bae599d1396b5556b51ac84b758a98f5e5d45ae7
longyahui <60100581+longyahui@users.noreply.github.com>
2022-12-22 10:25:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,10 +21,10 @@ setup(
     author = __author__,
     author_email = __author_email__,
     license = __license__,
-    packages = ['GraphST'],
+    packages = ["GraphST"],
     install_requires = __requires__,
     zip_safe = False,
     include_package_data = True,
     long_description = __long_description__,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown"
 )
